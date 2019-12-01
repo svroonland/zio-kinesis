1a681d2beedea901b5a62b9af4d0e6cffd0470b9
Steven Vroonland <steven.vroonland@ordina.nl>
2019-12-01 11:18:13 +0100

    Draft of queue-based Producer

--- src/main/scala/nl/vroste/zio/kinesis/client/Producer.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/Producer.scala
@@ -1,33 +1,36 @@
 package nl.vroste.zio.kinesis.client
 import nl.vroste.zio.kinesis.client.Client.ProducerRecord
 import nl.vroste.zio.kinesis.client.serde.Serializer
-import software.amazon.awssdk.services.kinesis.model.PutRecordsResultEntry
-import zio.{ Chunk, ZIO }
-import zio.stream.ZSink
+import zio.clock.Clock
+import zio.duration.{ Duration, _ }
+import zio.stream.{ ZSink, ZStream }
+import zio._
 
 import scala.collection.JavaConverters._
 
-object Producer {
-  case class Queue[T](record: ProducerRecord[T])
+/**
+ * Higher level functionality over PutRecords
+ *
+ * Handles batching, record aggregation, retrying in case of failure due to Kinesis rate limits.
+ *
+ * Has an internal queue
+ */
+trait Producer[T] {
+  def produce(r: ProducerRecord[T]): Task[Unit]
+  def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[Unit]
+}
 
-  /**
-   * Sink that produces records to Kinesis in an efficient and failsafe manner
-   *
-   * Records are buffered and sent in batches.
-   * Records that failed to produce are retried.
-   *
-   * @param streamName
-   * @param client
-   * @param serializer
-   * @tparam R
-   * @tparam T
-   * @return
-   */
-  def sink[R, T](
+object Producer {
+  case class ProduceRequest[T](r: ProducerRecord[T], done: Promise[Throwable, Unit])
+
+  def make[R, T](
     streamName: String,
     client: Client,
-    serializer: Serializer[R, T]
-  ): ZSink[R, Throwable, Nothing, Chunk[ProducerRecord[T]], Unit] = {
+    serializer: Serializer[R, T],
+    bufferSize: Int = 1024, // Prefer powers of 2
+    maxBufferDuration: Duration = 100.millis
+  ): ZManaged[R with Clock, Throwable, Producer[T]] = {
+
     // Make a putRecords call and return failed records
     def produce(records: Chunk[ProducerRecord[T]]): ZIO[R, Throwable, Chunk[ProducerRecord[T]]] =
       client.putRecords(streamName, serializer, records.toVector).map { response =>
@@ -47,6 +50,38 @@ object Producer {
       produce(failed)
     }
 
-    ZSink.drain.contramapM(produceWithRetry)
+    for {
+      queue <- zio.Queue.bounded[ProduceRequest[T]](bufferSize).toManaged(_.shutdown)
+      q     = queue.contramapM((r: ProducerRecord[T]) => Promise.make[Throwable, Unit].map(ProduceRequest(r, _)))
+
+      _ <- ZStream
+            .fromQueue(q)
+            .aggregateAsyncWithin(
+              ZSink.collectAllN[ProduceRequest[T]](bufferSize.toLong),
+              Schedule.spaced(maxBufferDuration)
+            ) // groupedWithin
+            .mapM { requests =>
+              // TODO it should retry together with the new incoming records
+              produceWithRetry(Chunk.fromIterable(requests).map(_.r)) *> ZIO.traverse(requests.map(_.done))(
+                _.succeed(())
+              )
+            }
+            .runDrain
+            .toManaged_
+            .fork
+    } yield new Producer[T] {
+      override def produce(r: ProducerRecord[T]): Task[Unit] =
+        for {
+          done    <- Promise.make[Throwable, Unit]
+          request = ProduceRequest(r, done)
+          _       <- queue.offer(request)
+          _       <- done.await
+        } yield ()
+
+      override def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[Unit] =
+        ZIO
+          .traverse(chunk.toVector)(r => Promise.make[Throwable, Unit].map(ProduceRequest(r, _)))
+          .flatMap(requests => queue.offerAll(requests) *> ZIO.foreach_(requests)(_.done.await))
+    }
   }
 }
