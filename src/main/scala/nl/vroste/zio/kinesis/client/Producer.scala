c7d61ec00efd092cab427ef44dce8100281d12e8
Steven Vroonland <steven.vroonland@ordina.nl>
2019-12-01 18:09:52 +0100

    Put failed requests back on the queue and batch them together with new incoming requests

--- src/main/scala/nl/vroste/zio/kinesis/client/Producer.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/Producer.scala
@@ -1,87 +1,112 @@
 package nl.vroste.zio.kinesis.client
 import nl.vroste.zio.kinesis.client.Client.ProducerRecord
+import nl.vroste.zio.kinesis.client.Producer.ProduceResponse
 import nl.vroste.zio.kinesis.client.serde.Serializer
+import zio._
 import zio.clock.Clock
 import zio.duration.{ Duration, _ }
-import zio.stream.{ ZSink, ZStream }
-import zio._
+import zio.stream.{ Sink, ZSink, ZStream }
 
 import scala.collection.JavaConverters._
 
 /**
- * Higher level functionality over PutRecords
+ * Producer for Kinesis records
  *
- * Handles batching, record aggregation, retrying in case of failure due to Kinesis rate limits.
+ * Performs batching of records for efficiency and retrying of failed put requests (eg due to Kinesis shard rate limits)
  *
  * Has an internal queue
+ *
+ * Inspired by https://docs.aws.amazon.com/streams/latest/dev/developing-producers-with-kpl.html
+ *
+ * Rate limits for PutRecords:
+ * - 500 records per request
+ * - Whole request max 5MB
+ * - Each item max 1MB
+ * - Each shard max 1000 records / s
+ * - Each shard max 1MB / s
  */
 trait Producer[T] {
-  def produce(r: ProducerRecord[T]): Task[Unit]
-  def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[Unit]
+
+  /**
+   * Put a record on
+   * Backpressures when too many requests are in flight
+   *
+   * @param r
+   * @return
+   */
+  def produce(r: ProducerRecord[T]): Task[ProduceResponse]
+
+  /**
+   * Backpressures when too many requests are in flight
+   */
+  def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[List[ProduceResponse]]
+
+  /**
+   * ZSink for producing records
+   */
+  def sinkChunked: ZSink[Any, Throwable, Nothing, Chunk[ProducerRecord[T]], Unit] = ZSink.drain.contramapM(produceChunk)
 }
 
 object Producer {
-  case class ProduceRequest[T](r: ProducerRecord[T], done: Promise[Throwable, Unit])
+  final case class ProduceResponse(shardId: String, sequenceNumber: String)
+
+  private case class ProduceRequest[T](r: ProducerRecord[T], done: Promise[Throwable, ProduceResponse])
 
   def make[R, T](
     streamName: String,
     client: Client,
     serializer: Serializer[R, T],
     bufferSize: Int = 1024, // Prefer powers of 2
-    maxBufferDuration: Duration = 100.millis
-  ): ZManaged[R with Clock, Throwable, Producer[T]] = {
-
-    // Make a putRecords call and return failed records
-    def produce(records: Chunk[ProducerRecord[T]]): ZIO[R, Throwable, Chunk[ProducerRecord[T]]] =
-      client.putRecords(streamName, serializer, records.toVector).map { response =>
-        val failed = Chunk
-          .fromIterable(
-            response
-              .records()
-              .asScala
-          )
-          .zipWith(records)(Tuple2.apply)
-          .collect { case (result, record) if result.errorCode() != null => record }
-
-        failed
-      }
-
-    def produceWithRetry(records: Chunk[ProducerRecord[T]]) = produce(records).flatMap { failed =>
-      produce(failed)
-    }
-
+    maxBufferDuration: Duration = 100.millis,
+    maxParallelRequests: Int = 24
+  ): ZManaged[R with Clock, Throwable, Producer[T]] =
     for {
       queue <- zio.Queue.bounded[ProduceRequest[T]](bufferSize).toManaged(_.shutdown)
-      q     = queue.contramapM((r: ProducerRecord[T]) => Promise.make[Throwable, Unit].map(ProduceRequest(r, _)))
+      q     = queue.contramapM((r: ProducerRecord[T]) => Promise.make[Throwable, ProduceResponse].map(ProduceRequest(r, _)))
 
-      _ <- ZStream
-            .fromQueue(q)
+      failedQueue <- zio.Queue.bounded[ProduceRequest[T]](bufferSize).toManaged(_.shutdown)
+
+      // Failed records get precedence)
+      _ <- (ZStream.fromQueue(failedQueue) merge ZStream.fromQueue(q))
+          // Buffer records up to maxBufferDuration or up to the Kinesis PutRecords request limit
             .aggregateAsyncWithin(
-              ZSink.collectAllN[ProduceRequest[T]](bufferSize.toLong),
+              Sink.collectAllN[ProduceRequest[T]](maxRecordsPerRequest.toLong),
               Schedule.spaced(maxBufferDuration)
-            ) // groupedWithin
-            .mapM { requests =>
-              // TODO it should retry together with the new incoming records
-              produceWithRetry(Chunk.fromIterable(requests).map(_.r)) *> ZIO.traverse(requests.map(_.done))(
-                _.succeed(())
-              )
+            )
+            // Several putRecords requests in parallel
+            .mapMPar(maxParallelRequests) { requests =>
+              for {
+                response <- client.putRecords(streamName, serializer, requests.map(_.r))
+
+                (newFailed, succeeded) = response
+                  .records()
+                  .asScala
+                  .zip(requests)
+                  .partition { case (result, _) => result.errorCode() != null }
+                _ <- failedQueue.offerAll(newFailed.map(_._2))
+                _ <- ZIO.traverse(succeeded) {
+                      case (response, request) =>
+                        request.done.succeed(ProduceResponse(response.shardId(), response.sequenceNumber()))
+                    }
+              } yield ()
             }
             .runDrain
             .toManaged_
             .fork
     } yield new Producer[T] {
-      override def produce(r: ProducerRecord[T]): Task[Unit] =
+      override def produce(r: ProducerRecord[T]): Task[ProduceResponse] =
         for {
-          done    <- Promise.make[Throwable, Unit]
-          request = ProduceRequest(r, done)
-          _       <- queue.offer(request)
-          _       <- done.await
-        } yield ()
+          done     <- Promise.make[Throwable, ProduceResponse]
+          request  = ProduceRequest(r, done)
+          _        <- queue.offer(request)
+          response <- done.await
+        } yield response
 
-      override def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[Unit] =
+      override def produceChunk(chunk: Chunk[ProducerRecord[T]]): Task[List[ProduceResponse]] =
         ZIO
-          .traverse(chunk.toVector)(r => Promise.make[Throwable, Unit].map(ProduceRequest(r, _)))
-          .flatMap(requests => queue.offerAll(requests) *> ZIO.foreach_(requests)(_.done.await))
+          .traverse(chunk.toVector)(r => Promise.make[Throwable, ProduceResponse].map(ProduceRequest(r, _)))
+          .flatMap(requests => queue.offerAll(requests) *> ZIO.traverse(requests)(_.done.await))
     }
-  }
+
+  val maxRecordsPerRequest = 500 // This is a Kinesis API limitation
 }
