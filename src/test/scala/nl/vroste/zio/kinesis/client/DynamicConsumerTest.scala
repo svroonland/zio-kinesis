bcde88a0512e0f4f6d6c48ee2036bbbef50db496
Steven Vroonland <steven.vroonland@ordina.nl>
2020-05-27 20:23:17 +0200

    Fix

--- src/test/scala/nl/vroste/zio/kinesis/client/DynamicConsumerTest.scala
+++ src/test/scala/nl/vroste/zio/kinesis/client/DynamicConsumerTest.scala
@@ -107,7 +107,7 @@ object DynamicConsumerTest extends DefaultRunnableSpec {
                         s"Checkpointing at offset ${sequenceNumberForShard} in consumer ${label}, shard ${shardId}"
                       ) *> r.checkpoint)
                         .when(sequenceNumberForShard % checkpointDivisor == checkpointDivisor - 1)
-                        .tapError(e => putStrLn(s"Failed to checkpoint in consumer ${label}, shard ${shardId}"))
+                        .tapError(_ => putStrLn(s"Failed to checkpoint in consumer ${label}, shard ${shardId}"))
                 }.map(_._1)
                   .as((label, shardId))
                   .ensuring(putStrLn(s"Shard $shardId completed for consumer $label"))
