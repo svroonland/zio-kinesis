72493594b50354e107bee142ca82584cd98c1b99
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2020-06-11 10:23:56 +0100

    ZLayers: rough draft for DynamicConsumer layering.

---
+++ src/main/scala/nl/vroste/zio/kinesis/client/clients.scala
@@ -0,0 +1,31 @@
+package nl.vroste.zio.kinesis.client
+
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import zio.{ Has, ZIO, ZLayer, ZManaged }
+
+object clients {
+
+  def kinesisAsyncClientLayer: ZLayer[Any, Throwable, Has[KinesisAsyncClient]] =
+    ZLayer.fromManaged(ZManaged.fromAutoCloseable {
+      ZIO.effect(
+        KinesisAsyncClient.builder.build
+      )
+    })
+
+  def cloudWatchAsyncClientLayer: ZLayer[Any, Throwable, Has[CloudWatchAsyncClient]] =
+    ZLayer.fromManaged(ZManaged.fromAutoCloseable {
+      ZIO.effect(
+        CloudWatchAsyncClient.builder.build
+      )
+    })
+
+  def dynamoDbAsyncClientLayer: ZLayer[Any, Throwable, Has[DynamoDbAsyncClient]] =
+    ZLayer.fromManaged(ZManaged.fromAutoCloseable {
+      ZIO.effect(
+        DynamoDbAsyncClient.builder.build
+      )
+    })
+
+}
