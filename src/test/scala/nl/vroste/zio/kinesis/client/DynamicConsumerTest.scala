5c893061f447621d34497c9da2ade29cf1d8d315
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2020-06-15 08:50:13 +0100

    ZLayers: try alternative consumer

--- src/test/scala/nl/vroste/zio/kinesis/client/DynamicConsumerTest.scala
+++ src/test/scala/nl/vroste/zio/kinesis/client/DynamicConsumerTest.scala
@@ -159,13 +159,70 @@ object DynamicConsumerTest extends DefaultRunnableSpec {
 
   def testCheckpointAtShutdown =
     testM("checkpoint for the last processed record at stream shutdown") {
-      val streamName = "zio-test-stream-" + UUID.randomUUID().toString
-//      val applicationName = "zio-test-" + UUID.randomUUID().toString
+      val streamName      = "zio-test-stream-" + UUID.randomUUID().toString
+      val applicationName = "zio-test-" + UUID.randomUUID().toString
 
       val batchSize = 100
       val nrBatches = 2 //100
       val records   =
         (1 to batchSize).map(i => ProducerRecord(s"key$i", s"msg$i"))
+
+      def streamConsumer(
+        workerIdentifier: String,
+        activeConsumers: Ref[Set[String]]
+      ): ZStream[Console with Blocking, Throwable, (String, String)] = {
+        val checkpointDivisor = 1
+
+        def handler(shardId: String, r: DynamicConsumer.Record[String]) =
+          for {
+            id <- ZIO.fiberId
+            _  <- putStrLn(s"Consumer $workerIdentifier on fiber $id got record $r on shard $shardId")
+            // Simulate some effectful processing
+            _  <- sleep(50.millis)
+          } yield ()
+
+        (for {
+          service <- ZStream.service[DynamicConsumer.Service]
+          stream  <- ZStream
+                      .fromEffect(putStrLn(s"Starting consumer $workerIdentifier"))
+                      .flatMap(_ =>
+                        service
+                          .shardedStream(
+                            streamName,
+                            applicationName = applicationName,
+                            deserializer = Serde.asciiString,
+                            workerIdentifier = applicationName + "-" + workerIdentifier,
+                            isEnhancedFanOut = false
+                          )
+                          .flatMapPar(Int.MaxValue) {
+                            case (shardId, shardStream, checkpointer @ _) =>
+                              shardStream.zipWithIndex.tap {
+                                case (r: DynamicConsumer.Record[String], sequenceNumberForShard: Long) =>
+                                  checkpointer.stageOnSuccess(handler(shardId, r))(r).as(r) <*
+                                    (putStrLn(
+                                      s"Checkpointing at offset ${sequenceNumberForShard} in consumer ${workerIdentifier}, shard ${shardId}"
+                                    ) *> checkpointer.checkpoint)
+                                      .when(sequenceNumberForShard % checkpointDivisor == checkpointDivisor - 1)
+                                      .tapError(_ =>
+                                        putStrLn(
+                                          s"Failed to checkpoint in consumer ${workerIdentifier}, shard ${shardId}"
+                                        )
+                                      )
+                              }.as((workerIdentifier, shardId))
+                                // Background and a bit delayed so we get a chance to actually emit some records
+                                .tap(_ => (sleep(1.second) *> activeConsumers.update(_ + workerIdentifier)).fork)
+                                .ensuring(putStrLn(s"Shard $shardId completed for consumer $workerIdentifier"))
+                                .catchSome {
+                                  case _: ShutdownException => // This will be thrown when the shard lease has been stolen
+                                    // Abort the stream when we no longer have the lease
+                                    ZStream.empty
+                                }
+                          }
+                      )
+
+        } yield stream)
+          .provideSomeLayer[Console with Blocking](LocalStackLayers.dynamicConsumerLayer)
+      }
 
 //      def streamConsumer(
 ////        interrupted: Promise[Nothing, Unit],
@@ -227,7 +284,11 @@ object DynamicConsumerTest extends DefaultRunnableSpec {
 //                           .tap(p => (putStrLn("INTERRUPTING") *> p.succeed(())).delay(19.seconds + 333.millis).fork)
 //          lastProcessedRecords    <- Ref.make[Map[String, String]](Map.empty) // Shard -> Sequence Nr
 //          lastCheckpointedRecords <- Ref.make[Map[String, String]](Map.empty) // Shard -> Sequence Nr
-//          _                         <- streamConsumer( /*interrupted, */ lastProcessedRecords, lastCheckpointedRecords)
+          activeConsumers <- Ref.make[Set[String]](Set.empty)
+          _               <- streamConsumer("consumer1", activeConsumers)
+                 .take(2L)
+                 .runCollect
+//          _               <- streamConsumer( /*interrupted, */ lastProcessedRecords, lastCheckpointedRecords)
 //                 .take(2L)
 //                 .runCollect
 //          _                         <- producing.interrupt
