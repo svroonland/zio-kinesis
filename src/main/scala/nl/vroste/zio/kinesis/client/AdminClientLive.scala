dce632dd227b09f1fae2fa875bdf6f1e2c6747cf
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2020-06-22 14:56:25 +0100

    ZLayers: deleted ExampleApp2.

--- src/main/scala/nl/vroste/zio/kinesis/client/AdminClientLive.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/AdminClientLive.scala
@@ -14,6 +14,7 @@ import zio.{ Tag => _, _ }
 import scala.jdk.CollectionConverters._
 
 object AdminClientLive {
+
   val layer: ZLayer[Has[KinesisAsyncClient], Throwable, AdminClient] =
     ZLayer.fromService[KinesisAsyncClient, AdminClient.Service] { kinesisClient =>
       new AdminClient.Service {
