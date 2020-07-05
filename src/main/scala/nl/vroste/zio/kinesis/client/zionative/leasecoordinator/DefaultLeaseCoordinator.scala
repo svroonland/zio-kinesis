d0f5170df96e38d7ccdf958f39660dbf69b1cf84
Steven Vroonland <svroonland@gmail.com>
2020-07-05 10:44:50 +0200

    Document lease coordination settings

--- src/main/scala/nl/vroste/zio/kinesis/client/zionative/leasecoordinator/DefaultLeaseCoordinator.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/zionative/leasecoordinator/DefaultLeaseCoordinator.scala
@@ -24,9 +24,14 @@ import zio.random.{ shuffle, Random }
 import zio.stream.ZStream
 
 /**
+ * Settings affecting lease taking, renewing and refreshing
+ *
  * Default values are compatible with KCL defaults (TODO not quite yet)
  *
  * @param expirationTime Time after which a lease is considered expired if not updated in the meantime
+ *                       This should be set to several times the lease renewal interval so that leases are
+ *                       not considered expired until several renew intervals have been missed, in case of
+ *                       transient connection issues.
  * @param refreshAndTakeInterval Interval at which leases are refreshed and possibly new leases taken
  * @param renewInterval Interval at which leases are renewed to prevent them expiring
  * @param maxParallelLeaseAcquisitions Maximum parallel calls to DynamoDB to claim a lease.
@@ -35,6 +40,7 @@ import zio.stream.ZStream
  * @param maxParallelLeaseRenewals Maximum parallel calls to DynamoDB to claim a lease.
  *                                     This is not the maximum number of leases to steal in one iteration
  *                                     of `refreshAndTakeInterval`.
+ * @param releaseLeaseTimeout Time after which releasing a lease is silently aborted.
  */
 case class LeaseCoordinationSettings(
   expirationTime: Duration = 10.seconds,
