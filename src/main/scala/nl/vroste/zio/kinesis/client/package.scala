f3dc89f1bf5bf12d610eb464db744d5adcfd5446
Steven Vroonland <svroonland@gmail.com>
2020-07-05 10:24:45 +0200

    Remove old httpClientBuilder

--- src/main/scala/nl/vroste/zio/kinesis/client/package.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/package.scala
@@ -1,10 +1,8 @@
 package nl.vroste.zio.kinesis
 
-import software.amazon.awssdk.http.nio.netty.{ Http2Configuration, NettyNioAsyncHttpClient }
 import software.amazon.awssdk.services.cloudwatch.{ CloudWatchAsyncClient, CloudWatchAsyncClientBuilder }
 import software.amazon.awssdk.services.dynamodb.{ DynamoDbAsyncClient, DynamoDbAsyncClientBuilder }
 import software.amazon.awssdk.services.kinesis.{ KinesisAsyncClient, KinesisAsyncClientBuilder }
-import zio.duration._
 import zio.{ Has, ZIO, ZLayer, ZManaged }
 
 package object client {
@@ -41,41 +39,4 @@ package object client {
         ZManaged.fromAutoCloseable(ZIO.effect(build(DynamoDbAsyncClient.builder().httpClient(client))))
       }
     }
-
-  /**
-   * Builder for SdkAsyncHttpClient that can be shared between the AWS SDK service clients
-   *
-   * Some settings' defaults are recommended settings for Kinesis streaming using HTTP 2
-   *
-   * @param maxConcurrency Maximum concurrent connections.
-   *                       Recommended to set higher than the amount of shards you expect to process
-   * @param initialWindowSize
-   * @param healthCheckPingPeriod
-   * @param maxPendingConnectionAcquires
-   * @param connectionAcquisitionTimeout
-   * @param readTimeout
-   * @return
-   */
-  def httpClientBuilder(
-    maxConcurrency: Int = Int.MaxValue,
-    initialWindowSize: Int = 512 * 1024, // 512 KB, see https://github.com/awslabs/amazon-kinesis-client/pull/706
-    healthCheckPingPeriod: Duration = 10.seconds,
-    maxPendingConnectionAcquires: Int = 10000,
-    connectionAcquisitionTimeout: Duration = 30.seconds,
-    readTimeout: Duration = 30.seconds
-  ) =
-    NettyNioAsyncHttpClient
-      .builder()
-      .maxConcurrency(maxConcurrency)
-      .connectionAcquisitionTimeout(connectionAcquisitionTimeout.asJava)
-      .maxPendingConnectionAcquires(maxPendingConnectionAcquires)
-      .readTimeout(readTimeout.asJava)
-      .http2Configuration(
-        Http2Configuration
-          .builder()
-          .initialWindowSize(initialWindowSize)
-          .maxStreams(maxConcurrency.toLong)
-          .healthCheckPingPeriod(healthCheckPingPeriod.asJava)
-          .build()
-      )
 }
