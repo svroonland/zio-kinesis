1745ac02175a6672d9470e55bd251516fcc1612c
svroonland <svroonland@users.noreply.github.com>
2020-08-21 20:18:06 +0200

    Restore dynamic schedule now that ZIO issue #4101 is fixed in ZIO 1.0.1 (#234)
    * Restore dynamic scheulde now that ZIO issue #4101 is fixed in ZIO 1.0.1
    
    * Fix broken tests

--- src/test/scala/nl/vroste/zio/kinesis/client/zionative/PollingFetcherTest.scala
+++ src/test/scala/nl/vroste/zio/kinesis/client/zionative/PollingFetcherTest.scala
@@ -224,7 +224,7 @@ object PollingFetcherTest extends DefaultRunnableSpec {
                   .shardRecordStream("shard1", ShardIteratorType.TrimHorizon)
                   .mapChunks(Chunk.single)
                   .tap(_ => chunksReceived.update(_ + 1))
-                  .take(nrBatches + 1)
+                  .take(nrBatches)
                   .runDrain
               }
               .provideSomeLayer[ZEnv with Logging with TestClock](
@@ -233,15 +233,11 @@ object PollingFetcherTest extends DefaultRunnableSpec {
               .fork
           _                         <- TestClock.adjust(0.seconds)
           chunksReceivedImmediately <- chunksReceived.get
-          _                          = println(chunksReceivedImmediately)
           _                         <- TestClock.adjust(5.second)
-          _                         <- TestClock.adjust(
-                 3.seconds
-               ) // TODO remove: because of broken dynamicSchedule and https://github.com/zio/zio/issues/4101
-          chunksReceivedLater <- chunksReceived.get
-          _                   <- chunksFib.join
+          chunksReceivedLater       <- chunksReceived.get
+          _                         <- chunksFib.join
         } yield assert(chunksReceivedImmediately)(equalTo(1L)) && assert(chunksReceivedLater)(
-          equalTo(nrBatches + 1)
+          equalTo(nrBatches)
         ))
       }
     ).provideCustomLayer(loggingEnv ++ TestClock.default)
