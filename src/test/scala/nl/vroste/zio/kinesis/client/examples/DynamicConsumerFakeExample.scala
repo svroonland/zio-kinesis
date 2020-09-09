bb912da860e1dd6f9be9a375284d8ea176a9a90b
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2020-09-09 07:23:02 +0100

    made primary stream error type Throwable.

--- src/test/scala/nl/vroste/zio/kinesis/client/examples/DynamicConsumerFakeExample.scala
+++ src/test/scala/nl/vroste/zio/kinesis/client/examples/DynamicConsumerFakeExample.scala
@@ -1,14 +1,11 @@
 package nl.vroste.zio.kinesis.client.examples
 
-import java.nio.ByteBuffer
-
-import nl.vroste.zio.kinesis.client.{ DynamicConsumer, _ }
 import nl.vroste.zio.kinesis.client.serde.Serde
+import nl.vroste.zio.kinesis.client.{ DynamicConsumer, _ }
 import zio._
 import zio.console.putStrLn
 import zio.duration._
 import zio.logging.slf4j.Slf4jLogger
-import zio.stream.ZStream
 
 /**
  * Basic usage example for `DynamicConsumerFake`
@@ -16,9 +13,7 @@ import zio.stream.ZStream
 object DynamicConsumerFakeExample extends zio.App {
   private val loggingLayer = Slf4jLogger.make((_, logEntry) => logEntry, Some(getClass.getName))
 
-  val shard: UIO[(String, ZStream[Any, Throwable, ByteBuffer])]                    =
-    DynamicConsumerFake.makeShard(Serde.asciiString, "testShard", List("msg1", "msg2"))
-  val shards: ZStream[Any, Nothing, (String, ZStream[Any, Throwable, ByteBuffer])] = ZStream.fromEffect(shard)
+  private val shards = DynamicConsumerFake.shardsFromIterable(Serde.asciiString, List("msg1", "msg2"))
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     for {
