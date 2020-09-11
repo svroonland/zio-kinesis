ab8c3a201dad385abe4658320c2daefc5986fec1
Steven Vroonland <svroonland@gmail.com>
2020-09-11 10:16:18 +0200

    Add record and payload sizes to metrics

--- src/main/scala/nl/vroste/zio/kinesis/client/producer/CurrentMetrics.scala
+++ src/main/scala/nl/vroste/zio/kinesis/client/producer/CurrentMetrics.scala
@@ -1,43 +1,51 @@
 package nl.vroste.zio.kinesis.client.producer
 import java.time.Instant
 
-import nl.vroste.zio.kinesis.client.producer.ProducerMetrics.{ emptyAttempts, emptyLatency }
-import org.HdrHistogram.{ Histogram, IntCountsHistogram }
+import nl.vroste.zio.kinesis.client.producer.ProducerMetrics.{
+  emptyAttempts,
+  emptyLatency,
+  emptyPayloadSizes,
+  emptyRecordSizes
+}
+import org.HdrHistogram.{ AbstractHistogram, Histogram, IntCountsHistogram }
 import zio.duration.Duration
 
 private[client] final case class CurrentMetrics(
   start: Instant,
-  published: IntCountsHistogram, // Tracks number of attempts
+  published: IntCountsHistogram,   // Tracks number of attempts
   nrFailed: Long,
   latency: Histogram,
-  shardPredictionErrors: Long
+  shardPredictionErrors: Long,
+  payloadSize: IntCountsHistogram, // PutRecords call histogram
+  recordSize: IntCountsHistogram   // PutRecords call histogram
 ) {
   val nrPublished = published.getTotalCount
 
-  def addSuccess(attempts: Int, latency1: Duration): CurrentMetrics = {
-    val newLatency = latency.copy()
-    newLatency.recordValue(latency1.toMillis max 0)
-
-    val newPublished = published.copy()
-    newPublished.recordValue(attempts.toLong)
-
-    copy(published = newPublished, latency = newLatency)
-  }
+  def addSuccess(attempts: Int, latency1: Duration): CurrentMetrics =
+    addSuccesses(Seq(attempts), Seq(latency1))
 
   def addFailures(nr: Int): CurrentMetrics =
     copy(nrFailed = nrFailed + nr)
 
-  def addSuccesses(publishedWithAttempts: Seq[Int], latencies: Seq[Duration]): CurrentMetrics = {
-    val newLatency = latency.copy()
-    latencies.map(_.toMillis max 0).foreach(newLatency.recordValue)
-
-    val newPublished = published.copy()
-    publishedWithAttempts.map(_.toLong).foreach(newPublished.recordValue)
-
-    copy(published = newPublished, latency = newLatency)
-  }
+  def addSuccesses(publishedWithAttempts: Seq[Int], latencies: Seq[Duration]): CurrentMetrics =
+    copy(
+      published = addToHistogram(published, publishedWithAttempts.map(_.toLong)),
+      latency = addToHistogram(latency, latencies.map(_.toMillis max 0))
+    )
 
   def addShardPredictionErrors(nr: Long): CurrentMetrics = copy(shardPredictionErrors = shardPredictionErrors + nr)
+
+  def addPayloadSize(size: Int): CurrentMetrics =
+    copy(payloadSize = addToHistogram(payloadSize, Seq(size.toLong)))
+
+  def addRecordSizes(sizes: Seq[Int]): CurrentMetrics =
+    copy(recordSize = addToHistogram(payloadSize, sizes.map(_.toLong)))
+
+  private def addToHistogram[T <: AbstractHistogram](hist: T, values: Seq[Long]): T = {
+    val newHist = hist.copy().asInstanceOf[T]
+    values.foreach(newHist.recordValue)
+    newHist
+  }
 }
 
 private[client] object CurrentMetrics {
@@ -47,6 +55,8 @@ private[client] object CurrentMetrics {
       published = emptyAttempts,
       nrFailed = 0,
       latency = emptyLatency,
-      shardPredictionErrors = 0
+      shardPredictionErrors = 0,
+      payloadSize = emptyPayloadSizes,
+      recordSize = emptyRecordSizes
     )
 }
