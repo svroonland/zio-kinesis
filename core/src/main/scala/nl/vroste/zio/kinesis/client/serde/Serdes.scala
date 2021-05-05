e8e4ab13ecf1c525701dd08e5cfc774201add806
svroonland <svroonland@users.noreply.github.com>
2021-05-05 17:05:42 +0200

    Build in some margin against too large records under aggregation (#503)
    * Random records
    
    * Build in some margin against too large records under aggregation
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- core/src/main/scala/nl/vroste/zio/kinesis/client/serde/Serdes.scala
+++ core/src/main/scala/nl/vroste/zio/kinesis/client/serde/Serdes.scala
@@ -2,11 +2,14 @@ package nl.vroste.zio.kinesis.client.serde
 
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
-
-import zio.ZIO
+import zio.{ Chunk, ZIO }
 
 private[serde] trait Serdes {
   val byteBuffer: Serde[Any, ByteBuffer] = Serde(ZIO.succeed(_))(ZIO.succeed(_))
+  val bytes: Serde[Any, Chunk[Byte]]     =
+    Serde(byteBuffer => ZIO.succeed(Chunk.fromByteBuffer(byteBuffer)))(chunk =>
+      ZIO.succeed(ByteBuffer.wrap(chunk.toArray))
+    )
   val asciiString: Serde[Any, String]    =
     byteBuffer.inmap(StandardCharsets.US_ASCII.decode(_).toString)(StandardCharsets.US_ASCII.encode)
 }
