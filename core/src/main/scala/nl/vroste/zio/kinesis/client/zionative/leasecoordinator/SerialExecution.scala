8c677812b2489729b7eb3ad696c2c6565c88b056
Steven Vroonland <svroonland@gmail.com>
2022-01-25 22:10:26 +0100

    Or with an STM to prevent some contention on the Semaphore map

--- core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/leasecoordinator/SerialExecution.scala
+++ core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/leasecoordinator/SerialExecution.scala
@@ -1,5 +1,6 @@
 package nl.vroste.zio.kinesis.client.zionative.leasecoordinator
-import zio.{ Ref, Semaphore, UManaged, ZIO }
+import zio.stm.TMap
+import zio.{ Semaphore, UManaged, ZIO }
 
 /**
  * Ensures that effects are run serially per key
@@ -14,27 +15,25 @@ trait SerialExecution[K] {
 object SerialExecution {
   def keyed[K]: UManaged[SerialExecution[K]] =
     for {
-      locks <- Ref.make(Map.empty[K, Semaphore]).toManaged_
+      locks <- TMap.empty[K, Semaphore].commit.toManaged_
     } yield new SerialExecution[K] {
       override def apply[R, E, A](key: K)(f: ZIO[R, E, A]): ZIO[R, E, A] =
         for {
-          lockOpt <- locks.get.map(_.get(key))
-          lock    <- lockOpt match {
-                       case Some(lock) => ZIO.succeed(lock)
-                       case None       =>
-                         for {
-                           // There's a race condition between getting a None from the locks map and putting the new Semaphore there, hence we
-                           // atomically check again. The unused created Semaphore will get GC'd
-                           newLock        <- Semaphore.make(1)
-                           newLockUpdated <- locks.modify { locks =>
-                                               locks.get(key) match {
-                                                 case Some(lock) => (lock, locks)
-                                                 case None       => (newLock, locks + (key -> newLock))
-                                               }
-                                             }
-                         } yield newLockUpdated
-                     }
+          lockOpt <- locks.get(key).commit
+          lock    <- lockOpt.fold(createLockForKey(key))(ZIO.succeed(_))
           result  <- lock.withPermit(f)
         } yield result
+
+      private def createLockForKey(key: K): ZIO[Any, Nothing, Semaphore] =
+        for {
+          // There's a race condition between getting a None from the locks map and putting the new Semaphore there, hence we
+          // atomically check again. The unused created Semaphore will get GC'd
+          newLock        <- Semaphore.make(1)
+          newLockUpdated <- (for {
+                              _              <- locks.putIfAbsent(key, newLock)
+                              newLockUpdated <- locks.getOrElse(key, newLock)
+                            } yield newLockUpdated).commit
+        } yield newLockUpdated
     }
+
 }
