64cd1d328e5271e5a8018b739e1d0b1d94f01615
Steven Vroonland <svroonland@gmail.com>
2023-04-26 12:59:26 +0200

    Fix issue with uneven nr of leases where they are not all taken

--- core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/ShardAssignmentStrategy.scala
+++ core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/ShardAssignmentStrategy.scala
@@ -137,7 +137,8 @@ object ShardAssignmentStrategy {
 
     val target = minTarget
 
-    val ourLeases = allLeases.filter(_.owner.contains(workerId))
+    val ourLeases     = allLeases.filter(_.owner.contains(workerId))
+    val unownedLeases = allLeases.filter(_.owner.isEmpty)
 
     val minNrLeasesToTake = Math.max(0, target - ourLeases.size)
     val maxNrLeasesToTake = Math.max(0, target + optional - ourLeases.size)
@@ -146,7 +147,7 @@ object ShardAssignmentStrategy {
     ZIO.logInfo(
       s"We have ${ourLeases.size}, we would like to have at least ${target}/${allLeases.size} leases (${activeWorkers.size} active workers, " +
         s"${zombieWorkers.size} zombie workers), we need ${minNrLeasesToTake} more with an optional ${optional}"
-    ) *> (if (minNrLeasesToTake > 0)
+    ) *> (if (minNrLeasesToTake > 0 || unownedLeases.nonEmpty)
             for {
               leasesWithoutOwner         <- shuffle(allLeases.filter(_.owner.isEmpty)).map(_.take(maxNrLeasesToTake))
               leasesExpired              <- shuffle(expiredLeases).map(_.take(maxNrLeasesToTake - leasesWithoutOwner.size))
