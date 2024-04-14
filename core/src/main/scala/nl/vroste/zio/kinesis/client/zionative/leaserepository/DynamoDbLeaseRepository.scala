cc8bd4bf6c8cdf8f5be10d8c48c1f2c129e3fe47
svroonland <svroonland@users.noreply.github.com>
2024-04-14 19:45:41 +0200

    Fix interruption issue related to use of timeout in operations called from finalizers (#957)
    * Upgrade to zio 2.0.20
    
    * Uncomment lines causing interrupted exceptions
    
    * Fix interruption issue

--- core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/leaserepository/DynamoDbLeaseRepository.scala
+++ core/src/main/scala/nl/vroste/zio/kinesis/client/zionative/leaserepository/DynamoDbLeaseRepository.scala
@@ -199,8 +199,8 @@ private class DynamoDbLeaseRepository(client: DynamoDb, settings: Settings) exte
     client
       .updateItem(request)
       .mapError(_.toThrowable)
-      // TODO timeouts used in finalizers cause interrupted exceptions..
-//      .timeoutFail(new TimeoutException(s"Timeout updating checkpoint"))(settings.timeout)
+      .disconnect
+      .timeoutFail(new TimeoutException(s"Timeout updating checkpoint"))(settings.timeout)
       .unit
       .catchAll {
         case _: ConditionalCheckFailedException =>
